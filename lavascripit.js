1a54e86938c46db9d15aee522d3317eebdde091a
Luís Gonga <luischimucomendes8@gmail.com>
2021-09-14 13:18:59 +0100

    Sclider com javascript puro.

---
+++ lavascripit.js
@@ -0,0 +1,14 @@
+var showed_box = 0;
+function fNext(){
+    showed_box += -500;
+    if(showed_box < -2000)
+    showed_box = 0;
+    document.getElementById('sid').style.transform = 'translateX('+showed_box +'px)';
+}
+
+function fPrevious(){
+    showed_box += 500;
+    if(showed_box > 0)
+    showed_box = -2000;
+    document.getElementById('sid').style.transform = 'translateX('+showed_box +'px)';
+}
